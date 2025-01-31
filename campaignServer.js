b84527d4bc9eac8d9fef16d0fe9eb8b23002402d
it <it@sece>
2025-01-31 11:34:15 +0530

    commit changes

--- campaignServer.js
+++ campaignServer.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 8002;
+const PORT = process.env.PORT || 8000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
